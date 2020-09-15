d5785edcac5b114516395bc7ae8996d4559f38d7
davidevnes <davidantunes422@gmail.com>
2020-09-16 00:39:13 +0400

    chore: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-// import { useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import { CssBaseline } from '@material-ui/core';
-// import IconButton from '@material-ui/core/IconButton';
-// import * as Icon from '@material-ui/icons';
 import Toolbar from '@material-ui/core/Toolbar';
-// import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { App, Main, Content } from './App.styled';
 import { Logo } from '~assets';
 import { Dashboard, Transactions, About, Search } from '~pages';
 
 export default () => {
-  // const location = useLocation();
-  // const theme = useTheme();
-  // const [mobileOpen, setMobileOpen] = React.useState(false);
-  // const handleDrawerToggle = () => {
-  //   setMobileOpen(!mobileOpen);
-  // };
-  // const matchesSmUp = useMediaQuery(theme.breakpoints.up('sm'));
-
   return (
     <App>
       <CssBaseline />
       <Main>
         <AppBar position="sticky" color="inherit">
           <Toolbar>
-            {/* {!matchesSmUp && (
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-              >
-                <Icon.Menu />
-              </IconButton>
-            )} */}
             <Logo />
           </Toolbar>
         </AppBar>
